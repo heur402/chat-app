80547f1a5617267342e0b584a9c5e6dacea9d042
BYIRINGIRO BONHEUR <byiringirobon01fra@gmail.com>
2026-04-27 10:17:02 +0200

    working with cloudinary

--- server/lib/cloudinary.js
+++ server/lib/cloudinary.js
@@ -1,4 +1,8 @@
 import { v2 as cloudinary } from "cloudinary";
+import { config } from "dotenv";
+
+// Explicitly load environment variables to avoid ES module hoisting issues
+config();
 
 cloudinary.config ({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
